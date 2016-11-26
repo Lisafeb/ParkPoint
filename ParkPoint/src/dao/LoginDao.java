da4cca04f4c53814d406f664f49c5b16ca03484c
Lisa Blaga <lisablaga@gmail.com>
2016-11-26 23:25:12 +0200

    login

--- ParkPoint/src/dao/LoginDao.java
+++ ParkPoint/src/dao/LoginDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LoginDao {
-    public static boolean validate(String name, String pass) {        
+    public static boolean validateEmail(String name) {        
         boolean status = false;
         Connection conn = null;
         PreparedStatement pst = null;
@@ -24,9 +25,9 @@ public class LoginDao {
                     .getConnection(url + dbName, userName, password);
 
             pst = conn
-                    .prepareStatement("select * from users where email=? and password=?");
+                    .prepareStatement("select email from users where email=?");
             pst.setString(1, name);
-            pst.setString(2, pass);
+            //pst.setString(2, pass);
 
             rs = pst.executeQuery();
             status = rs.next();
@@ -58,4 +59,64 @@ public class LoginDao {
         }
         return status;
     }
+    public static String validatePass(String email) {   
+    	
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "ParkPoint";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "admin";
+        String password = "test";
+        String value = null;
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager
+                    .getConnection(url + dbName, userName, password);
+
+            pst = conn.prepareStatement("select password from users where email=?");
+           
+            pst.setString(1, email);
+
+            rs = pst.executeQuery();
+            if (rs.next()){
+                value = rs.getString(1);
+                System.out.println("value");
+                System.out.println(value);
+        } 
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                	
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+		return value;
+		
+    }
+   
 }
